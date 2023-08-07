f6e0bbbbbd7d7c5499b2f4d1e6fe0bccd9f3890b
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-08-07 18:07:42 -0300

    H2 database, test profile, JPA

--- src/main/java/com/jhonerodrigues/springbootjpa/entities/User.java
+++ src/main/java/com/jhonerodrigues/springbootjpa/entities/User.java
@@ -3,23 +3,33 @@ package com.jhonerodrigues.springbootjpa.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
-	private String fone;
+	private String phone;
 	private String password;
 	
 	public User () {
 	}
 
-	public User(long id, String name, String email, String fone, String password) {
+	public User(long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
-		this.fone = fone;
+		this.phone = phone;
 		this.password = password;
 	}
 
@@ -47,12 +57,12 @@ public class User implements Serializable{
 		this.email = email;
 	}
 
-	public String getFone() {
-		return fone;
+	public String getPhone() {
+		return phone;
 	}
 
-	public void setFone(String fone) {
-		this.fone = fone;
+	public void setPhone(String fone) {
+		this.phone = fone;
 	}
 
 	public String getPassword() {
