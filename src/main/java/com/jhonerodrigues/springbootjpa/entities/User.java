bfb98cf278f4e0614cde6fc6ee4334a413ee75ef
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-08-07 17:40:20 -0300

    User entity and resource

---
+++ src/main/java/com/jhonerodrigues/springbootjpa/entities/User.java
@@ -0,0 +1,82 @@
+package com.jhonerodrigues.springbootjpa.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private long id;
+	private String name;
+	private String email;
+	private String fone;
+	private String password;
+	
+	public User () {
+	}
+
+	public User(long id, String name, String email, String fone, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.fone = fone;
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}	
+}
---
+++ src/main/java/com/jhonerodrigues/springbootjpa/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.jhonerodrigues.springbootjpa.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jhonerodrigues.springbootjpa.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1,"Maria","maria@gmail.com","41561161","senha123");
+		return ResponseEntity.ok().body(u);
+	}
+}
