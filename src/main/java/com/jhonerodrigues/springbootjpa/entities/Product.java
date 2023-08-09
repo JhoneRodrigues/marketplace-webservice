8035dfb15cf50caef2b8d066f52654ce670f855d
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-08-09 19:13:42 -0300

    Product

--- src/main/java/com/jhonerodrigues/springbootjpa/entities/Product.java
+++ src/main/java/com/jhonerodrigues/springbootjpa/entities/Product.java
@@ -13,8 +13,8 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
+@Table(name = "tb_products")
+public class Product implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
@@ -22,16 +22,22 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set <Category> categories = new HashSet<>();
 	
-	public Category() {
+	public Product () {
 	}
-	
-	public Category(Long id, String name) {
+
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -50,13 +56,37 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
-	}
-	
-	public Set<Product> getProducts() {
-		return products;
 	}
 
 	@Override
@@ -67,7 +97,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
 }
---
+++ src/main/java/com/jhonerodrigues/springbootjpa/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.jhonerodrigues.springbootjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jhonerodrigues.springbootjpa.entities.Product;
+import com.jhonerodrigues.springbootjpa.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById (Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}	
