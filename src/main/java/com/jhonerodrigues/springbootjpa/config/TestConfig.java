0549431c0a10efdcdc20469fdbff6c458491b7f2
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-08-14 11:05:26 -0300

    Payment, one-to-one association

--- src/main/java/com/jhonerodrigues/springbootjpa/config/TestConfig.java
+++ src/main/java/com/jhonerodrigues/springbootjpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jhonerodrigues.springbootjpa.entities.Category;
 import com.jhonerodrigues.springbootjpa.entities.Order;
 import com.jhonerodrigues.springbootjpa.entities.OrderItem;
+import com.jhonerodrigues.springbootjpa.entities.Payment;
 import com.jhonerodrigues.springbootjpa.entities.Product;
 import com.jhonerodrigues.springbootjpa.entities.User;
 import com.jhonerodrigues.springbootjpa.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/jhonerodrigues/springbootjpa/entities/Order.java
+++ src/main/java/com/jhonerodrigues/springbootjpa/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jhonerodrigues.springbootjpa.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,7 +29,7 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
@@ -39,6 +41,9 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set <OrderItem> itens = new HashSet<>();
+	
+	@OneToOne (mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -89,6 +94,14 @@ public class Order implements Serializable {
 		return Objects.hash(id);
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItens(){
 		return itens;	
 	}
